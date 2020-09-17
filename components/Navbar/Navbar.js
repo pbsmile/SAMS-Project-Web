2508a80329227308c0d81ed5a8f5b5b2cc46282e
KSPBDO <pb.thestar@gmail.com>
2020-09-17 23:09:49 +0700

    add navbar item + hover item

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,25 +2,26 @@ import React from "react";
 import MenuItems from "./MenuItem";
 
 const navbar = () => {
-  console.log("Menu",MenuItems)
-  console.log("Menu C Name",MenuItems.cName)
+  console.log("Menu", MenuItems);
+  
   return (
     <div>
       <nav className="Nav-Items">
         <h1 className="Nav-Logo">
           SAMS<i className="fas fa-people-carry"></i>
         </h1>
-        <div className="Nav-MenuIcon"></div>
-        <ul>
-          {/* {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={MenuItems.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })} */}
+        <ul className="Nav-Elements">
+          <li>
+            <a className="Nav-Home" href="#">
+              หน้าแรก
+            </a>
+          </li>
+          <li>
+            <a className="Nav-Activities" href="#">
+              กิจกรรมทั้งหมด
+            </a>
+          </li>
+          <div className="Nav-MenuIcon"></div>
         </ul>
       </nav>
     </div>
