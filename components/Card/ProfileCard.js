1727e9b32638030e46cb5f46b554136560513604
KSPBDO <pb.thestar@gmail.com>
2020-09-30 23:07:37 +0700

    change toggle + profile to sidebar

--- components/Card/ProfileCard.js
+++ components/Card/ProfileCard.js
@@ -9,31 +9,11 @@ import Typography from "@material-ui/core/Typography";
 
 const ProfileCard = () => {
     return (
-        <div className="Profile-Card-Div">
-      <nav className="Profile-Card-Nav">
-        <h1 className="Profile-Card-Nav-Popular">ประวัติส่วนตัว</h1>
-      </nav>
-      <Card className="Profile-Card">
-        <CardActionArea className="Profile-Card-Area">
-          <CardMedia
-            className="Profile-Card-Media"
-            image=""
-            title="Activity"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              60010549
-            </Typography>
-            <Typography gutterBottom variant="h5" component="h2">
-              เนตรชนก ปิยะพันธุ์
-            </Typography>
-            <Typography gutterBottom variant="h5" component="h2">
-              วิศวกรรมศาสตร์
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
+      <div className="Profile-Card-Div">
+        <p className="Profile-Card-Code">60010549</p>
+        <p className="Profile-Card-Name">เนตรชนก ปิยะพันธุ์</p>
+        <p className="Profile-Card-Department">วิศวกรรมศาสตร์</p>      
+      </div>
     )
 }
 
--- components/Toggle/ProfileToggle.js
+++ components/Toggle/ProfileToggle.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ProfileCard from "../Card/ProfileCard";
 
 const ProfileToggle = () => {
   const [toggle, setToggle] = useState("mypost");
@@ -33,61 +34,62 @@ const ProfileToggle = () => {
   };
 
   return (
-    <div className="Profile-Toggle-Div">
-      <div className="Profile-Toggle-Button-List">
-        <label
-          className={
-            toggle == "mypost"
-              ? "Profile-Toggle-Button-MyPost"
-              : "Profile-Toggle-Button-Trans"
-          }
-          onClick={() => handleClick("mypost")}
-        >
-          โพสต์ของฉัน
-        </label>
-        <label
-          className={
-            toggle == "favorite"
-              ? "Profile-Toggle-Button-Favorite"
-              : "Profile-Toggle-Button-Trans"
-          }
-          onClick={() => handleClick("favorite")}
-        >
-          ชื่นชอบ
-        </label>
-        <label
-          className={
-            toggle == "history"
-              ? "Profile-Toggle-Button-History"
-              : "Profile-Toggle-Button-Trans"
-          }
-          onClick={() => handleClick("history")}
-        >
-          ประวัติ
-        </label>
-      </div>
+    <div className="Profile-Toggle-Div Profile-Toggle-Button-List">
+      
+        <nav className="Profile-Toggle-Button-Menu active">
+          <ProfileCard />
+          <ul className="Profile-Toggle-Button-Items">
+            <label
+              className={
+                toggle == "mypost"
+                  ? "Profile-Toggle-Button-MyPost"
+                  : "Profile-Toggle-Button-Trans"
+              }
+              onClick={() => handleClick("mypost")}
+            >
+              โพสต์ของฉัน
+            </label>
+            <label
+              className={
+                toggle == "favorite"
+                  ? "Profile-Toggle-Button-Favorite"
+                  : "Profile-Toggle-Button-Trans"
+              }
+              onClick={() => handleClick("favorite")}
+            >
+              ชื่นชอบ
+            </label>
+            <label
+              className={
+                toggle == "history"
+                  ? "Profile-Toggle-Button-History"
+                  : "Profile-Toggle-Button-Trans"
+              }
+              onClick={() => handleClick("history")}
+            >
+              ประวัติ
+            </label>
+          </ul>
+        </nav>
       <div className="Profile-Toggle-List">
-        {toggle == "mypost" && (
-            <h1>My Post</h1>
-        )
-        //   order.map((item, index) => (
-        //     <CardOrder item={item} key={index} isComplete={_isComplete} />
-        //   ))
-    }
-        {toggle == "favorite" && (
-            <h1>Favorite</h1>
-        )
-        //   order.map((item, index) => (
-        //     <CardOrder item={item} key={index} isComplete={_isComplete} />
-        //   ))
-    }
-        {toggle == "history" && (
-            <h1>History</h1>
-        )
-        //   orderHistory.map((item, index) => (
-        //     <CardHistory item={item} key={index} />
-        //   ))
-    }
+        {
+          toggle == "mypost" && <h1>My Post</h1>
+          //   order.map((item, index) => (
+          //     <CardOrder item={item} key={index} isComplete={_isComplete} />
+          //   ))
+        }
+        {
+          toggle == "favorite" && <h1>Favorite</h1>
+          //   order.map((item, index) => (
+          //     <CardOrder item={item} key={index} isComplete={_isComplete} />
+          //   ))
+        }
+        {
+          toggle == "history" && <h1>History</h1>
+          //   orderHistory.map((item, index) => (
+          //     <CardHistory item={item} key={index} />
+          //   ))
+        }
       </div>
     </div>
   );
