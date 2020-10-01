1ff30fc11e1d4f6feb5200fd27faefe9364e327a
KSPBDO <pb.thestar@gmail.com>
2020-10-01 23:33:12 +0700

    Main Card + Nav Bar

---
+++ components/Slidebar/MainPageSlidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Slide } from "react-slideshow-image";
+import { Fade } from 'react-slideshow-image';
+import Slideimg1 from "../../Image/Slideimg1.jpg";
+
+const MainPageSlidebar = () => {
+
+  return (
+    <div className="">
+       {/* <Fade>
+        <div className="each-fade">
+          <div className="image-container">
+            <img src={Slideimg1} />
+          </div>
+          <h2>First Slide</h2>
+        </div>
+        <div className="each-fade">
+          <div className="image-container">
+            <img src={Slideimg1} />
+          </div>
+          <h2>Second Slide</h2>
+        </div>
+        <div className="each-fade">
+          <div className="image-container">
+            <img src={Slideimg1} />
+          </div>
+          <h2>Third Slide</h2>
+        </div>
+      </Fade> */}
+    </div>
+  );
+};
+
+export default MainPageSlidebar;
