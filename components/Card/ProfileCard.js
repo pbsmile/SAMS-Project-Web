9ad74070bde491897b9080b862e6de93c8619743
KSPBDO <pb.thestar@gmail.com>
2020-10-09 00:37:56 +0700

    add icon + edit toggle profile style

--- components/Card/ProfileCard.js
+++ components/Card/ProfileCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -8,13 +8,13 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const ProfileCard = () => {
-    return (
-      <div className="Profile-Card-Div">
-        <p className="Profile-Card-Code">60010549</p>
-        <p className="Profile-Card-Name">เนตรชนก ปิยะพันธุ์</p>
-        <p className="Profile-Card-Department">วิศวกรรมศาสตร์</p>      
-      </div>
-    )
-}
+  return (
+    <div className="Profile-Card-Div">
+      <p className="Profile-Card-Code">60010549</p>
+      <p className="Profile-Card-Name">เนตรชนก ปิยะพันธุ์</p>
+      <p className="Profile-Card-Department">วิศวกรรมศาสตร์</p>
+    </div>
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
