317cf6bde5274b7d57ed35c1e79eed5a40228c91
KSPBDO <pb.thestar@gmail.com>
2020-10-27 14:12:22 +0700

    Query User Profile

--- components/Card/ProfileCard.js
+++ components/Card/ProfileCard.js
@@ -6,13 +6,31 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+export const QUERY_USERPROFILE = gql`
+  query {
+    getOneUser {
+      name
+      studentId
+      major
+    }
+  }
+`;
+
 
 const ProfileCard = () => {
+  const { data, loading, error } = useQuery(QUERY_USERPROFILE)
+  if (error) return <p>Ooobs...something went wrong, please try again later.</p>
+  if (loading) return <p>Loading...</p>
+  console.log(data.getOneUser)
+
   return (
     <div className="Profile-Card-Div">
-      <p className="Profile-Card-Code">60010549</p>
-      <p className="Profile-Card-Name">เนตรชนก ปิยะพันธุ์</p>
-      <p className="Profile-Card-Department">วิศวกรรมศาสตร์</p>
+      <p className="Profile-Card-Code">{data.getOneUser.studentId}</p>
+      <p className="Profile-Card-Name">{data.getOneUser.name}</p>
+      <p className="Profile-Card-Department">{data.getOneUser.major}</p>
     </div>
   );
 };
