bfab064d4f0323647e40194409730a042e3d9f25
pawornlak <powornlak@gmail.com>
2021-02-03 18:21:15 +0700

    double slide Login+Register Page

--- components/NewLogin/NewLogin.js
+++ components/NewLogin/NewLogin.js
@@ -2,66 +2,85 @@ import React, { useEffect, useRef, useState, Children, createContext, useContext
 import { useMutation } from "@apollo/react-hooks";
 import Router from "next/router";
 import { render } from "react-dom";
-// import SlidingPane from "react-sliding-pane";
-// import "react-sliding-pane/dist/react-sliding-pane.css";
+import login from "../../Image/login.png";
 
-
+const useEventListener = (target, type, listener, ...options) => {
+    React.useEffect(
+        () => {
+            const targetIsRef = target.hasOwnProperty("current");
+            const currentTarget = targetIsRef ? target.current : target;
+            if (currentTarget)
+                currentTarget.addEventListener(type, listener, ...options);
+            return () => {
+                if (currentTarget)
+                    currentTarget.removeEventListener(type, listener, ...options);
+            };
+        },
+        [target, type, listener, options]
+    );
+};
 
 const NewLogin = () => {
 
-    const [state, setState] = useState({
-        container: 'right-panel-active'
-      });
+    // const signUp = useState('signUp');
+    // const signIn = useState('signIn');
+    // const container = useState('container');
+
+    const signUp = React.useRef(null);
+    const signIn = React.useRef(null);
+    const container = React.useRef(null);
+    useEventListener(signIn, "click", () => container.current.classList.remove("right-panel-active"));
+    useEventListener(signUp, "click", () => container.current.classList.add("right-panel-active"));
 
     return (
-
-        <div className="container" id="container">
-            <div className="form-container sign-up-container">
-                <form action="#">
-                    <h1>Create Account</h1>
-                    <div className="social-container">
-                        <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                        <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                        <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                    </div>
-                    <span>or use your email for registration</span>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button>Sign Up</button>
-                </form>
-            </div>
-            <div className="form-container sign-in-container">
-                <form action="#">
-                    <h1>Sign in</h1>
-                    <div className="social-container">
-                        <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                        <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                        <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                    </div>
-                    <span>or use your account</span>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <a href="#">Forgot your password?</a>
-                    <button>Sign In</button>
-                </form>
-            </div>
-            <div className="overlay-container">
-                <div className="overlay">
-                    <div className="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button className="ghost" id="signIn"onClick={() => setState({ container: 'right-panel-active' })} >Sign In</button>
-                    </div>
-                    <div className="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button className="ghost" id="signUp" onClick={() => setState({ container: 'right-panel-active' })}>Sign Up</button>
+        <div className="login_body">
+            <div className="container" id="container" ref={container}>
+                <div className="form-container sign-up-container">
+                    <form action="#">
+                        <h1>Create Account</h1>
+                        <div className="social-container">
+                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Sign in</h1>
+                        <div className="social-container">
+                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <span>or use your account</span>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forgot your password?</a>
+                        <button>Sign In</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button className="ghost" id="signIn" ref={signIn}>Sign In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button className="ghost" id="signUp" ref={signUp}>Sign Up</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
 
         // <div classNameName="cont">
         //     <div className="form sign-in">
@@ -130,7 +149,6 @@ const NewLogin = () => {
         //         </div>
         //     </div>
         // </div>
-
     );
 };
 export default NewLogin;
--- pages/newLogin.js
+++ pages/newLogin.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import apolloClient from "../apollo/apolloClient"
-import Navbar from '../components/Navbar/Navbar'
 import NewLogin from '../components/NewLogin/NewLogin'
-import Login from '../components/NewLogin/NewLogin'
-// import Login from '../components/NewLogin/NewLogin'
-
-
 
 
 const newLogin = () => {
