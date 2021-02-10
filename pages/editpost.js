cb615892627808313a05d44069de11cdae72d7e2
pawornlak <powornlak@gmail.com>
2021-02-10 17:31:26 +0700

    editActivity page

---
+++ pages/editpost.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import EditActivity from '../components/PostActivity/EditPost'
+
+const editActivity = () => {
+    return (
+        <div>
+            <Navbar/>
+            
+            <EditActivity/>
+        </div>
+    )
+}
+
+export default editActivity;
