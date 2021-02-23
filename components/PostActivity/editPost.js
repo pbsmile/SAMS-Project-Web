bd5dae6a7e3cbd761a4465051d10a12c1944b656
pawornlak <powornlak@gmail.com>
2021-02-23 22:50:26 +0700

    major change can edit

--- components/PostActivity/editPost.js
+++ components/PostActivity/editPost.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useContext, createContext } from "react";
+import React, {useState} from "react";
 import { AuthContext } from "../../appState/AuthProvider";
 import { useQuery } from "@apollo/react-hooks";
 import { useRouter } from "next/router";
-import { Route, Switch } from "react-router";
+
 import Router from "next/router";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
@@ -10,11 +10,7 @@ import Link from "next/link";
 
 import CreateAct from "../../Image/create.png"
 import ImageLogo from "../../Image/img.png"
-import TextField from '@material-ui/core/TextField';
-import Card from "@material-ui/core/Card";
 
-
-import Datetime from 'react-datetime';
 
 const EDITPOST = gql`
 mutation EDITPOST(
@@ -77,36 +73,21 @@ const EditPost = () => {
     // const [radio, setRadio] = useState(null);
     // const [NumofPerson, setNumofPerson] = useState(null);
 
-    console.log("testrouter")
+
     const route = useRouter();
     console.log(route);
     const postId = route.query.activityId;
-    
-    // const { data, loading, error } = useQuery(QUERY_ACTIVITY, {
-    //     variables: { postId },
-    //     onCompleted: (data) => {
-    //         if (data) {
-    //             // console.log("JOIN", data.getOnePost.canJoin);
-    //             console.log("data");
-    //             console.log(data.getOnePost.name);
-    //             // userInfo()
-    //             //Router.push("/activity");
-    //         }
-    //     },
-    // });
-
     const { data, loading, error } = useQuery(QUERY_ACTIVITY, {
         variables: { postId },
+        onCompleted: (data) => {
+            if (data) {
+                // console.log("JOIN", data.getOnePost.canJoin);
+                console.log(data.getOnePost.name);
+                // userInfo()
+                //Router.push("/activity");
+            }
+        },
     });
-    if (data) {
-        console.log("have data")
-        console.log(data)
-    }
-
-    console.log("pass oncomplete");
-
-    
-    if (loading) return <p>Loading ...</p>;
 
     console.log("postId", postId);
 
@@ -127,14 +108,10 @@ const EditPost = () => {
     // console.log(dateFormat(data.getOnePost.dateCloseApply,"yyyy-mm-dd'T'HH:MM"))
     // if (error) return <p>Something went wrong, please try again.</p>;
 
-    // else if (loading) return <p>Loading ...</p>;
-    console.log("before test" )
-    const [test, settest] = useState("null");
-    console.log("test" + test)
-    
+    if (loading) return <p>Loading ...</p>;
+
     const [userInfo, setUserInfo] = useState({
-        // photo: "data.getOnePost.photo",
-        photo: data.getOnePost.photo,
+        photo: "",
         name: data.getOnePost.name,
         dateStart: dateFormat(data.getOnePost.dateStart, "isoDate"),
         dateEnd: dateFormat(data.getOnePost.dateEnd, "isoDate"),
@@ -147,19 +124,24 @@ const EditPost = () => {
         description: data.getOnePost.description,
     });
 
-    // const [userInfo, setUserInfo] = useState(data.getOnePost.major)
-
     console.log(userInfo)
+    const handleChange = e => {
+        console.log("Value", e.target.value)
+        setUserInfo({
+            ...userInfo,
 
-    
+            [e.target.name]: e.target.value
+        })
+    }
+
 
 
     const [EditPost] = useMutation(EDITPOST, {
         variables: { postId, ...userInfo },
         //เมื่อสำเร็จแล้วจะส่ง data เอามาใช้ได้
-        onCompleted: (dataset) => {
-            if (dataset) {
-                console.log(dataset);
+        onCompleted: (data2) => {
+            if (data2) {
+                console.log(data2);
                 setUserInfo({
                     photo: "",
                     name: "",
@@ -173,29 +155,20 @@ const EditPost = () => {
                     major: "",
                     description: "",
                 });
-
+                
             }
-            Router.push('/main');
+            Router.push('/activity/'+ postId);
             console.log("on complete")
             console.log(userInfo)
         },
-
+        
 
     })
 
-    
-    const handleChange = e => {
-        console.log("Value", e.target.value)
-        setUserInfo({
-            ...userInfo,
-            [e.target.name]: e.target.value
-        })
-    }
 
-    const handleSubmit = async () => {
+    const handleSubmit = async e => {
         console.log(userInfo)
         console.log("handle submit")
-        // await EditPost();
         try {
             console.log("Doneeeeeeeeeee1")
             e.preventDefault();
@@ -207,9 +180,10 @@ const EditPost = () => {
             console.log(error);
         }
     };
-    const handleUnSubmit = async () => {
-        Router.push('/main');
-    };
+    // const handleSubmit = async () => {
+    //     console.log("handle submit")
+    //     await editpost()
+    // };
 
     //image
     const [picture, setPicture] = useState(null);
@@ -230,17 +204,10 @@ const EditPost = () => {
 
     // Set Drop down and radio
     const [major, setMajor] = useState(null);
-    // const [status, setStatus] = useState(null)
+    const [status, setStatus] = useState(null)
     const [radio, setRadio] = useState(null);
-    // const [NumofPerson, setNumofPerson] = useState(null);
-    // const [shouldRender, setShouldRender] = useState(true);
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setShouldRender(false);
-    //     }, 5000);
-    // }, []);
+    const [NumofPerson, setNumofPerson] = useState(null);
 
-    // if( !shouldRender ) return null;
     return (
         <div className="Post-Page" >
             <form className="Post-Page" onSubmit={handleSubmit}>
@@ -280,7 +247,7 @@ const EditPost = () => {
                         <div className="Post-Column2 Post-Input">
                             <div className="Post-Flex-Row">
                                 <form noValidate className="Post-Calendar-Time">
-                                    <input type="date" name="dateStart" data-date-format="MM-DD-YYY" className="Post-Input-Fill-Data" onChange={handleChange} value={userInfo.dateStart} />
+                                    <input type="date" name="dateStart" InputLabelProps={{ shrink: true, }} data-date-format="MM-DD-YYY" className="Post-Input-Fill-Data" onChange={handleChange} value={userInfo.dateStart} />
 
                                     {/* <TextField
                                         id="date"
@@ -297,7 +264,7 @@ const EditPost = () => {
                                 </form>
                                 <h3 className="Post-Calendar-Time">ถึง</h3>
                                 <form noValidate className="Post-Calendar">
-                                    <input type="date" name="dateEnd" data-date-format="MM-DD-YYY" className="Post-Input-Fill-Data" onChange={handleChange} value={userInfo.dateEnd} />
+                                    <input type="date" name="dateEnd" InputLabelProps={{ shrink: true, }} data-date-format="MM-DD-YYY" className="Post-Input-Fill-Data" onChange={handleChange} value={userInfo.dateEnd} />
                                     {/* <TextField
                                         id="date"
                                         name="dateEnd"
