1e64f5bfd718a84255212fb2d040122385b59845
pawornlak <powornlak@gmail.com>
2021-03-04 00:46:44 +0700

    attendance map

--- components/AttendanceCheck/AttendanceCheck.js
+++ components/AttendanceCheck/AttendanceCheck.js
@@ -6,17 +6,69 @@ import { useMutation } from "@apollo/react-hooks";
 import Link from "next/link";
 import Router from "next/router";
 
-import {Button,Modal} from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
+
+
+// const ATTENDANCECHECK = gql`
+//   mutation ATTENDANCECHECK($postId: String!,$checkedUsersId: [String!]){
+//     attendanceCheck(input:{
+//       postId:$postId,
+//       checkedUsersId:$checkedUsersId
+//     })
+//     {
+//       checkedUsers{
+//         name
+//       }
+//     }
+//   }
+// `;
+
+const QUERY_ACTIVITY = gql`
+  query QUERY_ACTIVITY($postId: String!) {
+    getOnePost(input: { postId: $postId }) {
+      name
+      _id
+      joinUsers{_id,name,studentId}
+      checkedUsers{_id, name}
+    }
+  }
+`;
 
 const AttendanceCheck = () => {
 
+    const route = useRouter();
+    console.log(route);
+    const postId = route.query.activityId;
+    const [attendCheck, setattendCheck] = useState("");
+
+    console.log("attendCheck : ", attendCheck);
+
+    const { data, loading, error } = useQuery(QUERY_ACTIVITY, {
+        variables: { postId },
+        onCompleted: (data) => {
+            if (data) {
+                console.log("data1 : " + data.getOnePost.joinUsers[0].name)
+                console.log("data2 : " + data.getOnePost.joinUsers[0].studentId)
+
+            }
+        },
+    });
+    if (error) return <p>Something went wrong, please try again.</p>;
+    if (loading) return <p>Loading ...</p>;
 
     return (
         <div>
-            
+            Activities Page
+            for web
+            SAMS
+            {data.getOnePost.joinUsers.map((prod) => (
+                <div key={prod._id}>
+                   {prod.name} 
+                </div>
+            ))}
+            {/* {data.getOnePost.joinUsers[0].name} */}
         </div>
-
     );
 };
 
