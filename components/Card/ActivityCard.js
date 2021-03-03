00f363ca27b673e2f0f12d974773abacab5b3f5b
KSPBDO <pb.thestar@gmail.com>
2021-03-03 23:20:11 +0700

    Map card act form search(name)

--- components/Card/ActivityCard.js
+++ components/Card/ActivityCard.js
@@ -58,6 +58,11 @@ const ActivityCard = () => {
   const [toggleFav, setToggleFav] = useState("unfav");
   console.log("Fav>>", toggleFav);
 
+  const [value, setValue] = useState();
+  const [valueYear, setValueYear] = useState();
+  const [valueMajor, setValueMajor] = useState();
+  const [valueStatus, setValueStatus] = useState();
+
   const dateFormat = require("dateformat");
   dateFormat.i18n = {
     dayNames: [
@@ -128,7 +133,51 @@ const ActivityCard = () => {
         <nav className="Activity-Page-Card-Nav">
           <p className="Activity-Page-Card-Nav-Popular">กิจกรรมทั้งหมด</p>
         </nav>
-        <Filter />
+        <div>
+          <div className="Filter-Items-Div">
+            <nav className="Filter-Items Filter-Items-Flex">
+              <select className="Filter-Years" id="Filter-Years">
+                <option value="0000">ปีการศึกษา</option>
+                <option value="2561">2561</option>
+                <option value="2562">2562</option>
+                <option value="2563">2563</option>
+              </select>
+              <select className="Filter-Departments" id="Filter-Departments">
+                <option value="0">เลือกคณะ/วิทยาลัย</option>
+                <option value="1">คณะวิศวกรรมศาสตร์</option>
+                <option value="2">คณะสถาปัตยกรรมศาสตร</option>
+                <option value="3">คณะครุศาสตร์อุตสาหกรรมและเทคโนโลยี</option>
+                <option value="4">คณะวิทยาศาสตร์</option>
+                <option value="5">คณะเทคโนโลยีการเกษตร </option>
+                <option value="6">คณะเทคโนโลยีสารสนเทศ</option>
+                <option value="7">คณะการบริหารและการจัดการ</option>
+                <option value="8">คณะศิลปศาสตร์</option>
+                <option value="9">คณะแพทยศาสตร์</option>
+                <option value="10">
+                  วิทยาลัยนาโนเทคโนโลยีพระจอมเกล้าลาดกระบัง
+                </option>
+                <option value="11">วิทยาลัยนวัตกรรมการผลิตขั้นสูง</option>
+                <option value="12">วิทยาลัยอุตสาหกรรมการบินนานาชาติ</option>
+                <option value="13">วิทยาลัยวิจัยนวัตกรรมทางการศึกษา</option>
+                <option value="14">วิทยาลัยวิศวกรรมสังคีต</option>
+              </select>
+              <select className="Filter-Statuses" id="Filter-Filter-Statuses">
+                <option value="Status">สถานะโครงการ</option>
+                <option value="Open">เปิดรับสมัคร</option>
+                <option value="Close">ปิดรับสมัคร</option>
+                <option value="Limites">เต็มจำนวนรับ</option>
+              </select>
+            </nav>
+            <input
+              className="Filter-Search-Bar"
+              placeholder="ค้นหากิจกรรม"
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        {/* <Filter /> */}
       </div>
 
       <div className="Activity-Page-Card-List">
@@ -139,56 +188,70 @@ const ActivityCard = () => {
                 <h4>{prod.name}</h4>
               </div>
             ))} */}
-            {data.getAllPostsByAuthen.posts.map((prod) => (
-              <div key={prod._id}>
-                <Card className="Activity-Page-Card">
-                  <div className="Activity-Page-Card-Area">
-                    <div className="Activity-Page-Card-Flex">
-                      <div className="Activity-Page-Card-Left">
-                        <Link
-                          key={prod._id}
-                          href="/activity/[activityId]"
-                          as={`/activity/${prod._id}`}
-                        >
-                          <img className="Activity-Page-Card-Img" src={Chest} />
-                        </Link>
-                        {/* <label className="Activity-Page-Card-Status">
+            {data.getAllPostsByAuthen.posts
+              .filter((item) => {
+                if (!value) return true;
+                if (
+                  item._id.includes(value) ||
+                  (item.name.includes(value))
+                ) {
+                  return true;
+                }
+              })
+              .map((prod) => (
+                <div key={prod._id}>
+                  <Card className="Activity-Page-Card">
+                    <div className="Activity-Page-Card-Area">
+                      <div className="Activity-Page-Card-Flex">
+                        <div className="Activity-Page-Card-Left">
+                          <Link
+                            key={prod._id}
+                            href="/activity/[activityId]"
+                            as={`/activity/${prod._id}`}
+                          >
+                            <img
+                              className="Activity-Page-Card-Img"
+                              src={Chest}
+                            />
+                          </Link>
+                          {/* <label className="Activity-Page-Card-Status">
                           สถานะกิจกรรม :
                         </label>
                         <label className="Activity-Page-Card-Status"> {prod.status} </label> */}
-                      </div>
-                      <div className="Activity-Page-Card-Right">
-                        <div className="Activity-Page-Card-Date-Time">
-                          <label className="Activity-Page-Card-Date">
-                            {/* <img
+                        </div>
+                        <div className="Activity-Page-Card-Right">
+                          <div className="Activity-Page-Card-Date-Time">
+                            <label className="Activity-Page-Card-Date">
+                              {/* <img
                               className="Activity-Page-Card-Icon-Size"
                               src={Day}
                             /> */}
-                            {dateFormat(prod.dateStart, "dddd d mmmm yyyy")} - {prod.timeStart} น.
-                            {/* <Moment format="D MMM YYYY">
+                              {dateFormat(prod.dateStart, "dddd d mmmm yyyy")} -{" "}
+                              {prod.timeStart} น.
+                              {/* <Moment format="D MMM YYYY">
                               {prod.dateStart}
                             </Moment> */}
-                          </label>
-                          {/* <label className="Activity-Page-Card-Time">
+                            </label>
+                            {/* <label className="Activity-Page-Card-Time">
                             <img
                               className="Activity-Page-Card-Icon-Size"
                               src={Time}
                             />
                             {prod.timeStart} น.
                           </label> */}
-                        </div>
-                        <label className="Activity-Page-Card-Name">
-                          {prod.name}
-                        </label>
+                          </div>
+                          <label className="Activity-Page-Card-Name">
+                            {prod.name}
+                          </label>
 
-                        <label className="Activity-Page-Card-Location">
-                          <img
-                            className="Activity-Page-Card-Icon-Size"
-                            src={Location}
-                          />
-                          {prod.place}
-                        </label>
-                        {/* <div className="Activity-Page-Card-Members-Close">
+                          <label className="Activity-Page-Card-Location">
+                            <img
+                              className="Activity-Page-Card-Icon-Size"
+                              src={Location}
+                            />
+                            {prod.place}
+                          </label>
+                          {/* <div className="Activity-Page-Card-Members-Close">
                           <label className="Activity-Page-Card-Members">
                             <img
                               className="Activity-Page-Card-Icon-Size"
@@ -204,10 +267,10 @@ const ActivityCard = () => {
                             {prod.dateCloseApply}
                           </label>
                         </div> */}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  {/* <CardActions>
+                    {/* <CardActions>
                     <div className="Activity-Page-Card-More-Div">
                       <Link
                         key={prod._id}
@@ -220,9 +283,9 @@ const ActivityCard = () => {
                       </Link>
                     </div>
                   </CardActions> */}
-                </Card>
-              </div>
-            ))}
+                  </Card>
+                </div>
+              ))}
           </>
         )}
       </div>
